c3a1d8c28f8e3c8ca33a21ca437b0976a3341142
Helen Akimova <me4ta18@gmail.com>
2014-06-16 14:55:05 -0700

    Created class Node

--- LinkedList.py
+++ LinkedList.py
@@ -1,1 +1,25 @@
 #implement LinkedList class here
+
+#initialize elements with object and link onto the next element
+#add element to the end of list
+#delete element by index
+#insert element
+#fined element
+
+
+class Node:
+    def __init__(self, value = None, next_node = None):
+        self.value = value
+        self.next_node = next_node
+
+    def __repr__(self):
+        return '%s' % self.value
+
+
+
+
+#element1 = Node(5)
+#print(element1)
+#element2 = Node(6, element1)
+#print(element2, element2.next_element)
+
